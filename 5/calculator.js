e1020bfbd7bed57b7b0768bd40f163df9c1e0dbb
ArtN7 <112850344+ArtN7@users.noreply.github.com>
2022-10-10 09:40:26 +0300

    Update calculator.js

--- 5/calculator.js
+++ 5/calculator.js
@@ -8,7 +8,7 @@ function countPrice(event) {
     const count = document.getElementById("count");
     const total = document.getElementById("total");
     let result = price.value * count.value;
-    if (!Number.isNaN(result)) {
+    if (!Number.isNaN(result)&&price.value>0&&count.value>0) {
         total.value = result;
     } else {
         alert("Ошибка при вводе данных!");
